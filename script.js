b3957af0722db8c4f0ceea754910d683b8f000af
unknown <matej.pavic92@gmail.com>
2020-12-30 12:07:41 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const deg = 6; //360deg / 60(min||sec) => 6
+const hr = document.querySelector("#hr");
+const mn = document.querySelector("#mn");
+const sc = document.querySelector("#sc");
+/*var digits = {};
+
+    var positions = ['h1', 'h2', ':', 'm1', 'm2', ':', 's1', 's2' ];
+
+    var digitHolder = clock.find('.digits');
+
+    $.each(positions, function(){
+*/
+setInterval(() => {
+  let day = new Date();
+  let hh = day.getHours() * 30; //360deg / 12 hour => 30
+  let mm = day.getMinutes() * deg;
+  let ss = day.getSeconds() * deg;
+/*var digits = {};
+
+    var positions = ['h1', 'h2', ':', 'm1', 'm2', ':', 's1', 's2' ];
+
+    var digitHolder = clock.find('.digits');
+
+    $.each(positions, function(){
+*/
+  hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+});
